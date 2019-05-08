dfb0e1f74ccf66a7fa7cb40b5472a598edcd665e
yangqiong <yangqionggo@163.com>
2019-05-09 07:33:12 +0800

    graphql-playlist #21 before react client

--- graphql-playlist/server/app.js
+++ graphql-playlist/server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/graphql-playlist')
+mongoose.connection.once('open', () => {
+  console.log('connnected to database')
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
