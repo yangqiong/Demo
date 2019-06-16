b6dd1ac19d813511eff62ba30259e556cd86dba0
yang <yang@MacBook.local>
2019-06-16 16:52:41 +0800

    第一章 入门

---
+++ gopl.io/ch01/dup.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	// input := bufio.NewScanner(os.Stdin)
+	// for input.Scan() {
+	// 	counts[input.Text()]++
+	// }
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Println("%s\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
